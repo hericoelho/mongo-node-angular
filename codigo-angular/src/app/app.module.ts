20bf3f96a7ce4b075f0bb9653ebc985c397f2400
Herivelton Coelho <hericoelho@gmail.com>
2020-04-30 20:00:05 -0300

    authentication with fake server

--- codigo-angular/src/app/app.module.ts
+++ codigo-angular/src/app/app.module.ts
@@ -1,24 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { JwtInterceptor } from './_helpers/jwt.interceptor';
+import { ErrorInterceptor } from './_helpers/error.interceptor';
 
 //fake-backend
-
 import { fakeBackendProvider } from './_helpers/fake-backend';
+
+import { AlertComponent } from './_components/alert/alert.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     LoginComponent,
-    RegisterComponent
+    RegisterComponent,
+    AlertComponent
   ],
   imports: [
     BrowserModule,
@@ -27,6 +31,8 @@ import { fakeBackendProvider } from './_helpers/fake-backend';
     HttpClientModule,
   ],
   providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
     // provider used to create fake backend
     fakeBackendProvider
   ],
--- codigo-angular/src/app/home/home.component.ts
+++ codigo-angular/src/app/home/home.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { first } from 'rxjs/operators';
+
+import { UserService } from '../_services/user.service';
+import { AuthenticationService } from '../_services/authentication.service';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +10,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  currentUser: any;
+  users = [];
 
-  constructor() { }
+  constructor(
+    private authenticationService: AuthenticationService,
+    private userService: UserService
+) {
+    this.currentUser = this.authenticationService.currentUserValue;
+}
 
   ngOnInit(): void {
+    this.loadAllUsers();
   }
+
+  deleteUser(id: number) {
+    this.userService.delete(id)
+        .pipe(first())
+        .subscribe(() => this.loadAllUsers());
+}
+
+private loadAllUsers() {
+    this.userService.getAll()
+        .pipe(first())
+        .subscribe(users => this.users = users);
+}
 
 }
